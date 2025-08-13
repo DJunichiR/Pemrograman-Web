eb7656750ba2c6ad5c01b320e37b623064b7532f
DJunichiR <144043036+DJunichiR@users.noreply.github.com>
2025-08-13 13:50:03 +0700

    Add files via upload

---
+++ beranda.blade.php
@@ -0,0 +1,200 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Junichi's CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+
+
+    <title>Dapa's Porto</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-p fixed-top" style="background-color: #925c70";>
+        <div class="container-fluid">
+            <a class="navbar-brand ms-1" href="#">Junichi</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="navbarNav">
+                <ul class="navbar-nav ms-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/beranda">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- End Navbar -->
+
+    <!-- Jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="assets/YL 4.jpg" class="rounded-circle img-thumbnail" alt="Mas Lex" width="200" />
+        <h1 class="display-6">Samuel Alexander the 3rd</h1>
+        <p class="lead">Rapper | Programmer | Developer</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffffff" fill-opacity="0.7"
+                d="M0,96L48,96C96,96,192,96,288,101.3C384,107,480,117,576,144C672,171,768,213,864,240C960,267,1056,277,1152,256C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
+    </section>
+
+
+    <!-- About -->
+    <section id="about>
+        <div class="container">
+        <div class="row mb-3 text-center">
+            <div class="col">
+                <h2>About Me</h2>
+            </div>
+        </div>
+
+        <div class="row justify-content-center fs-5 text-center">
+            <div class="col-md-4 mb-3">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo alias velit autem! Eum officia
+                    cum cumque nesciunt ea fuga amet. </p>
+            </div>
+            <div class="col-md-4 mb-3">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid inventore nobis ut nihil excepturi
+                    debitis quasi rem vel, veritatis voluptas?</p>
+            </div>
+        </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#d8ccd1" fill-opacity="1"
+                d="M0,192L11.4,208C22.9,224,46,256,69,229.3C91.4,203,114,117,137,106.7C160,96,183,160,206,181.3C228.6,203,251,181,274,149.3C297.1,117,320,75,343,85.3C365.7,96,389,160,411,170.7C434.3,181,457,139,480,106.7C502.9,75,526,53,549,80C571.4,107,594,181,617,213.3C640,245,663,235,686,197.3C708.6,160,731,96,754,64C777.1,32,800,32,823,64C845.7,96,869,160,891,186.7C914.3,213,937,203,960,197.3C982.9,192,1006,192,1029,165.3C1051.4,139,1074,85,1097,90.7C1120,96,1143,160,1166,186.7C1188.6,213,1211,203,1234,181.3C1257.1,160,1280,128,1303,133.3C1325.7,139,1349,181,1371,176C1394.3,171,1417,117,1429,90.7L1440,64L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z">
+            </path>
+        </svg>
+    </section>
+
+    <!-- Akhir About -->
+
+    <!-- Projects -->
+    <section id="projects">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    <h2>My Projects</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="assets/Maslex.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="assets/Maslex.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="assets/Maslex.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1"
+                d="M0,192L11.4,208C22.9,224,46,256,69,229.3C91.4,203,114,117,137,106.7C160,96,183,160,206,181.3C228.6,203,251,181,274,149.3C297.1,117,320,75,343,85.3C365.7,96,389,160,411,170.7C434.3,181,457,139,480,106.7C502.9,75,526,53,549,80C571.4,107,594,181,617,213.3C640,245,663,235,686,197.3C708.6,160,731,96,754,64C777.1,32,800,32,823,64C845.7,96,869,160,891,186.7C914.3,213,937,203,960,197.3C982.9,192,1006,192,1029,165.3C1051.4,139,1074,85,1097,90.7C1120,96,1143,160,1166,186.7C1188.6,213,1211,203,1234,181.3C1257.1,160,1280,128,1303,133.3C1325.7,139,1349,181,1371,176C1394.3,171,1417,117,1429,90.7L1440,64L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z">
+            </path>
+        </svg>
+
+    </section>
+    <!-- Akhir Projects -->
+
+    <!-- Contact -->
+    <section id="contact">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    <h2>Contact Me</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form>
+                        <div class="mb-3">
+                            <label for="nama" class="form-label">Nama</label>
+                            <input type="nama" class="form-control" id="nama" aria-describedby="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pesan" class="form-label">Pesan</label>
+                            <textarea class="form-control" id="pesan" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Kirim</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#925c70" fill-opacity="1"
+                d="M0,96L48,96C96,96,192,96,288,101.3C384,107,480,117,576,144C672,171,768,213,864,240C960,267,1056,277,1152,256C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
+    </section>
+    <!-- Akhir Contact -->
+
+    <!-- Footer -->
+    <footer class="footer fw-bold pb-5 text-center text-white">
+        <p> -- Mas Lex -- <a href="https://www.instagram.com/young_lex18/" class="fw-bold text-white">YL</a></p>
+    </footer>
+    <!-- Akhir Footer -->
+
+    <!-- End Jumbotron -->
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
